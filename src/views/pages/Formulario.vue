82cf7c7568802f7daad448092f931500b3e4320e
Nícolas Ribeiro Saliba <saliban3@gmail.com>
2025-04-18 00:17:56 -0300

    Alteraçao de nome do arquivo de formulário

---
+++ src/views/pages/Formulario.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container fluid ma-0 pa-0>
+      <HelloWorld/>
+      <v-container
+        fluid ma-0 pa-0 xs12 md12 
+        class="d-flex flex-column flex-md-row flex-lg-row flex-xl-row"
+      >
+        <v-flex xs12 md7 lg7 class="">
+          <AboutFacig/>
+        </v-flex>
+        <v-flex xs12 md5 lg5 flex-column secondary class="">
+          <MidiaArticles/>
+          <SocialMidia/>
+        </v-flex>
+      </v-container>
+      <Apareca/>
+    </v-container>
+  </template>
+  
